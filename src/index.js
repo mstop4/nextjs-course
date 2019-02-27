59efc2f4dc9afe881ff214dcafe5f55ffe03b0af
M.S.T.O.P <mstop4@gmail.com>
2019-02-27 01:12:46 -0500

    class component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-const Hello = (props) => {
-  return React.createElement('h1',
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString()
+    }
+  }
+
+  tick() {
+    this.setState(() => {
+      return ({
+        time: new Date().toLocaleString()
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return React.createElement('h1',
     {className: 'orange'},
-    'Hello From Pluralsight and React ' + props.time);
+    'Hello From Pluralsight and React ' + this.state.time);
+  }
 };
 
 ReactDOM.render(
